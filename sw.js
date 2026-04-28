ae1fe5e6050e4f7f5a92e69564113f27254ef753
marcoxxx72-cmyk <marcoxxx72@gmail.com>
2026-04-28 17:01:36 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+// Service worker - clears all caches and unregisters
+self.addEventListener('install', function() { self.skipWaiting(); });
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+    }).then(function() { return self.clients.claim(); })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request));
+});
